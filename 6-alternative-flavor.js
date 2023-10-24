0306e132b7f8f23fdd720ee08a6ccd8c7ee607ed
Hilal Fakherdine <hilalfeld@gmail.com>
2023-10-24 20:44:27 +0300

    first commit

---
+++ 6-alternative-flavor.js
@@ -0,0 +1,9 @@
+// if I need to export items I can do the following istead
+// module.exports.items = ["item1", "item2"];
+
+const items = ["item1", "item2"];
+const person = {
+  name: "bob",
+};
+
+module.exports.singlePerson = person;
